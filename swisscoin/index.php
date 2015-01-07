b6fcfb3700495a0f1dc0b957e4d38fbcd2138efb
syntime <armadil@yandex.ru>
2015-01-07 03:40:01 +0300

    form

--- swisscoin/index.php
+++ swisscoin/index.php
@@ -208,15 +208,99 @@
             </form>
             <h1 class="text-center">1AwfZ7xJL6w7rm33RDLZQxn6HoYhztAQgW</h1>
             <?
-                if (isset ($_POST['name']) && isset ($_POST['email']) && isset ($_POST['txid'])) {
-                    mail ($_POST['email'],
-                          "The SwissCoin",
-                          "Thank you for support.\n\nWe are very pleased that you are interested in our project. The number of coins you purchased is recorded in our users registry. We hasten to remind you that the IPO will be held on 13th of January, 2015.\nFollow our website."
-                    );
-                    echo ('<p style="color: green">Thank you for support!</p>');
-                    $_POST['name'] = $_POST['email'] = $_POST['txid'] = '';
-                }
-            ?>
+				if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['txid'])) {
+					
+					$to = "armadil@yandex.ru";
+					$nameto = "Who To";
+					$from = "noreply@switzcoin.com";
+					$namefrom = "Who From";
+					$subject = "The SwissCoin";
+					$message = "Thank you for support.\n\nWe are very pleased that you are interested in our project. The number of coins you purchased is recorded in our users registry. We hasten to remind you that the IPO will be held on 13th of January, 2015.\nFollow our website.";
+					authSendEmail($from, $namefrom, $to, $nameto, $subject, $message);
+					
+					echo ('<p style="color: green">Thank you for support! Check your mail ($_POST['email']) for details.</p>');
+				} else {
+					echo ('<p style="color: red">Error. Mail dont send.</p>');
+				}
+
+				function authSendEmail($from, $namefrom, $to, $nameto, $subject, $message)
+				{
+					//SMTP + SERVER DETAILS
+					/* * * * CONFIGURATION START * * * */
+					$smtpServer = "mail.ukdns.biz";
+					$port = "25";
+					$timeout = "30";
+					$username = "your-email-address@domain.com";
+					$password = "Your-POP3-Password";
+					$localhost = "mail.ukdns.biz";
+					$newLine = "\r\n";
+					/* * * * CONFIGURATION END * * * * */
+
+					//Connect to the host on the specified port
+					$smtpConnect = fsockopen($smtpServer, $port, $errno, $errstr, $timeout);
+					$smtpResponse = fgets($smtpConnect, 515);
+					if(empty($smtpConnect))
+					{
+						$output = "Failed to connect: $smtpResponse";
+						return $output;
+					}
+					else
+					{
+						$logArray['connection'] = "Connected: $smtpResponse";
+					}
+
+					//Request Auth Login
+					fputs($smtpConnect,"AUTH LOGIN" . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['authrequest'] = "$smtpResponse";
+
+					//Send username
+					fputs($smtpConnect, base64_encode($username) . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['authusername'] = "$smtpResponse";
+
+					//Send password
+					fputs($smtpConnect, base64_encode($password) . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['authpassword'] = "$smtpResponse";
+
+					//Say Hello to SMTP
+					fputs($smtpConnect, "HELO $localhost" . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['heloresponse'] = "$smtpResponse";
+
+					//Email From
+					fputs($smtpConnect, "MAIL FROM: $from" . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['mailfromresponse'] = "$smtpResponse";
+
+					//Email To
+					fputs($smtpConnect, "RCPT TO: $to" . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['mailtoresponse'] = "$smtpResponse";
+
+					//The Email
+					fputs($smtpConnect, "DATA" . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['data1response'] = "$smtpResponse";
+
+					//Construct Headers
+					$headers = "MIME-Version: 1.0" . $newLine;
+					$headers .= "Content-type: text/html; charset=iso-8859-1" . $newLine;
+					$headers .= "To: $nameto <$to>" . $newLine;
+					$headers .= "From: $namefrom <$from>" . $newLine;
+
+					fputs($smtpConnect, "To: $to\nFrom: $from\nSubject: $subject\n$headers\n\n$message\n.\n");
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['data2response'] = "$smtpResponse";
+
+					// Say Bye to SMTP
+					fputs($smtpConnect,"QUIT" . $newLine);
+					$smtpResponse = fgets($smtpConnect, 515);
+					$logArray['quitresponse'] = "$smtpResponse";
+				}
+			?>
+
             <form role="form" class="form" method="POST" id="feedback-form">
                 <img class="qr block" src="img/qr.gif">
                 <img class="price block" src="img/price.png">
