9dcf1aac2ea8ccf96977426a4264172770dded69
syntime <armadil@yandex.ru>
2015-07-13 01:33:25 +0300

    some cute cubes 2

--- edge-planet/shaders/fshader.glsl
+++ edge-planet/shaders/fshader.glsl
@@ -1,11 +1,11 @@
 precision mediump float;
 
-uniform vec4 uColor;
+uniform vec3 uColor;
 uniform vec3 uLight;
 
 varying vec3 vWorldSpaceNormal;
 varying vec3 vPos;
 
 void main(void) {
-    gl_FragColor = vec4(vPos * dot(uLight, vWorldSpaceNormal) * 0.5 + 0.5 * uColor.rgb, uColor.w);
+    gl_FragColor = vec4(uColor * dot(uLight, vWorldSpaceNormal) * 0.5 + 0.5 * uColor, 1.0);
 }
